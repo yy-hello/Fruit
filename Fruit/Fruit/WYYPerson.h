4654d367cc54501ec8ae94a84c9dfe25c45e1b4b
WYY <916431540@qq.com>
2017-04-28 17:01:15 +0800

    开始开发1.0版本并创建person类

---
+++ Fruit/Fruit/WYYPerson.h
@@ -0,0 +1,13 @@
+//
+//  WYYPerson.h
+//  Fruit
+//
+//  Created by 吴阳阳 on 2017/4/28.
+//  Copyright © 2017年 吴阳阳. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WYYPerson : NSObject
+
+@end
