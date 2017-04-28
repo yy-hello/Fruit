5d96a8bb84c262de65ce94d5af1ba555dcb86652
WYY <916431540@qq.com>
2017-04-28 16:58:06 +0800

    初始化项目

---
+++ Fruit/Fruit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Fruit
+//
+//  Created by 吴阳阳 on 2017/4/28.
+//  Copyright © 2017年 吴阳阳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Fruit/Fruit/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Fruit
+//
+//  Created by 吴阳阳 on 2017/4/28.
+//  Copyright © 2017年 吴阳阳. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
